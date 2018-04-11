760ed9f6e0bec08dffd186955f2d2b2fcf65420d
Hrishikesh Rangnekar <hrishikesh.rangnekar@cumulus-systems.com>
2018-04-11 11:36:41 +0530

    hot fix done

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,19 +1,7 @@
 package test;
 
 public class Main {
-	//hotfix changes done 2.0
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		// a fix for issue
-		
-		// some more changes
-		
-		// changes for 53
-		// cahnges 53 2.0
-		
-		// c1
-		
-		//c2
+		// hot fix changes made
 	}
 }
